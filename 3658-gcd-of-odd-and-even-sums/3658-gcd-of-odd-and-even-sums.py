fd2631e146ed2b870fedb7cf6550107040eae289
shambhaviasole <161708561+shambhaviasole@users.noreply.github.com>
2026-07-15 23:15:10 +0530

    Time: 57 ms (10.85%), Space: 19.3 MB (21.29%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -0,0 +1,11 @@
+import math
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        odd=[i for i in range(1,n*2) if i%2==1]
+        sumOdd=sum(odd)
+        even=[i for i in range(1,n*2) if i%2==0]
+        sumEven=sum(even)
+        while sumEven != 0:
+            sumOdd,sumEven = sumEven, sumOdd%sumEven
+        return abs(sumOdd)
+        #return math.gcd(sumOdd,sumEven)
